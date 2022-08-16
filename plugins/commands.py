3be7d2f584f6d7f1daf5cd1606b696a26d84748b
betterbot69 <110813024+betterbot69@users.noreply.github.com>
2022-08-16 20:42:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'https://t.me/moviesbighubgroup/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Join Our Movies Request Group ➕', url=f'https://t.me/moviesbighubgroup/')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/+Hnxo-lpGfLEzOTc1')
@@ -88,7 +88,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'https://t.me/moviesbighubgroup/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Join Our Movies Request Group ➕', url=f'https://t.me/moviesbighubgroup/')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/+Hnxo-lpGfLEzOTc1')
