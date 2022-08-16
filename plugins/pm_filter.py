19cdf6587162dce11e63fb68d2d23d7e01443523
betterbot69 <110813024+betterbot69@users.noreply.github.com>
2022-08-16 22:11:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -404,9 +404,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            
+            InlineKeyboardButton('➕ Join Our Movies Request Group ➕', url='https://t.me/moviesbighubgroup')
         ], [
-            InlineKeyboardButton('Join Our Movies Request Group', url='https://t.me/moviesbighubgroup')
             InlineKeyboardButton('🤖 Updates', url='https://t.me/+Hnxo-lpGfLEzOTc1')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
